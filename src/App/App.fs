54c8a990653dc935bbf4d22d966939f4b55527fb
Erik Rasmussen <ecr.referee@hotmail.com>
2023-05-03 22:43:32 -0500

    Recreate the app from scratch

--- src/App/App.fs
+++ src/App/App.fs
@@ -2,91 +2,33 @@ module App
 
 open Browser
 open Sutil
-open Sutil.Bulma
-open Sutil.CoreElements
-open type Feliz.length // For CSS units like px
 
-DomHelpers.setHeadTitle Dom.document "UnstoppableMango"
-
-let versions = {|
-    bulma = "0.9.4"
-    bulmaPrefersDark = "0.1.0-beta.1"
-    fontAwesome = "6.3.0"
-|}
+let versions = {| fontAwesome = "6.3.0" |}
 
 [
-    $"https://cdnjs.cloudflare.com/ajax/libs/bulma/{versions.bulma}/css/bulma.min.css"
-    $"https://cdnjs.cloudflare.com/ajax/libs/bulma-prefers-dark/{versions.bulmaPrefersDark}/bulma-prefers-dark.min.css"
     $"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/{versions.fontAwesome}/css/fontawesome.min.css"
     $"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/{versions.fontAwesome}/css/brands.min.css"
     $"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/{versions.fontAwesome}/css/solid.min.css"
+    "tailwind.css"
 ]
 |> List.iter (DomHelpers.setHeadStylesheet Dom.document)
 
+DomHelpers.setHeadTitle Dom.document "UnstoppableMango"
+
 let app () =
-    bulma.hero [
-        color.isPrimary
-        hero.isBold
-        hero.isFullheightWithNavbar
-        bulma.heroHead [
-            bulma.navbar [
-                navbar.isFixedTop
-                color.hasBackgroundDark
-                bulma.container [
-                    bulma.navbarBrand.div [
-                        bulma.navbarItem.a [
-                            bulma.text.span (Html.text "UnstoppableMango")
-                        ]
+    Html.divc "h-screen bg-cover bg-center lg:bg-right bg-[url(images/hbg-md.webp)] bg-byzantium-200" [
+        Html.divc "h-full grid content-center" [
+            Html.divc "w-full py-5 backdrop-blur bg-gradient-to-r from-transparent via-[rgb(0,0,0,0.75)] to-transparent" [
+                Html.divc "h-full flex flex-wrap items-center justify-center gap-6" [
+                    Html.imgc "h-48 rounded-full" [
+                        Attr.src "images/panigel.png"
                     ]
-                    bulma.navbarMenu [
-                        bulma.navbarEnd.div [
-                            bulma.navbarItem.div [
-                                bulma.button.a [
-                                    color.hasTextBlack
-                                    Attr.href "https://github.com/UnstoppableMango"
-                                    bulma.icon (Html.ic "fab fa-github" [])
-                                    Html.span "GitHub"
-                                ]
-                            ]
+                    Html.divc "flex flex-col justify-center items-center font-mono" [
+                        Html.spanc "text-4xl lg:text-5xl text-thistle-50 py-1" [
+                            text "UnstoppableMango"
                         ]
-                    ]
-                ]
-            ]
-        ]
-        bulma.heroBody [
-            bulma.title.h1 "UnstoppableMango IO"
-            bulma.subtitle.p "yeet"
-            bulma.columns [
-                bulma.column [
-                    bulma.card [
-                        color.hasBackgroundDanger
-                        bulma.cardHeader [
-                            bulma.cardHeaderTitle.p "Test"
-                        ]
-                        bulma.cardContent [
-                            Html.text "test"
-                        ]
-                    ]
-                ]
-                bulma.column [
-                    bulma.card [
-                        color.hasBackgroundInfo
-                        bulma.cardHeader [
-                            bulma.cardHeaderTitle.p "Test"
-                        ]
-                        bulma.cardContent [
-                            Html.text "test"
-                        ]
-                    ]
-                ]
-                bulma.column [
-                    bulma.card [
-                        color.hasBackgroundPrimary
-                        bulma.cardHeader [
-                            bulma.cardHeaderTitle.p "Test"
-                        ]
-                        bulma.cardContent [
-                            Html.text "test"
+                        Html.spanc "lg:text-xl text-thistle-200 py-2" [
+                            text "Developer | Metalhead | Idiot"
                         ]
                     ]
                 ]
