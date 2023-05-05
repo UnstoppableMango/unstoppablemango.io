d61ede30661203eb404dd64b7c981a75911499a3
Erik Rasmussen <ecr.referee@hotmail.com>
2023-05-04 22:27:53 -0500

    Add custom domain

--- infra/index.ts
+++ infra/index.ts
@@ -1,5 +1,6 @@
 import * as pulumi from '@pulumi/pulumi';
 import * as azure from '@pulumi/azure-native';
+import * as cloudflare from '@pulumi/cloudflare';
 
 const config = new pulumi.Config();
 const repositoryToken = config.requireSecret('repositoryToken');
@@ -23,6 +24,23 @@ const site = new azure.web.StaticSite('app', {
     skipGithubActionWorkflowGeneration: true,
     githubActionSecretNameOverride: 'AZURE_STATIC_WEB_APPS_API_TOKEN',
   },
+});
+
+const customDomain = new azure.web.StaticSiteCustomDomain(
+  'unstoppablemango.io',
+  {
+    name: site.name,
+    resourceGroupName: resourceGroup.name,
+    domainName: 'unstoppablemango.io',
+  }
+);
+
+const primaryTxtRecord = new cloudflare.Record('unstoppablemango.io', {
+  name: customDomain.domainName,
+  zoneId: 'de10a9e5057761cf8b2151d80dd684fa',
+  value: customDomain.validationToken,
+  type: 'TXT',
+  comment: 'Validation record for Azure',
 });
 
 export const url = pulumi.interpolate`https://${site.defaultHostname}`;
