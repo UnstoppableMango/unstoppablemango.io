21aaebc67e35ed823e6819f00248a002713b0d83
Erik Rasmussen <ecr.referee@hotmail.com>
2023-05-06 20:06:13 -0500

    Setup redirects

--- infra/index.ts
+++ infra/index.ts
@@ -10,6 +10,9 @@ const resourceGroup = new azure.resources.ResourceGroup(resourceGroupName, {
   resourceGroupName,
 });
 const domainName = 'unstoppablemango.io';
+const mangioDomainName = 'unstoppablemang.io';
+const unstoppableMangoZoneId = 'de10a9e5057761cf8b2151d80dd684fa';
+const unstoppableMangZoneId = '24f3d181ad1b22a5e290175096171516';
 
 const site = new azure.web.StaticSite('app', {
   resourceGroupName: resourceGroup.name,
@@ -42,10 +45,60 @@ const customDomain = new azure.web.StaticSiteCustomDomain(
 
 const primaryCnameRecord = new cloudflare.Record('unstoppablemango.io-cname', {
   name: domainName,
-  zoneId: 'de10a9e5057761cf8b2151d80dd684fa',
+  zoneId: unstoppableMangoZoneId,
   value: site.defaultHostname,
   type: 'CNAME',
   proxied: true,
+});
+
+const unstoppablemangoSettings = new cloudflare.ZoneSettingsOverride(
+  'unstoppablemango.io',
+  {
+    zoneId: unstoppableMangoZoneId,
+    settings: {
+      ssl: 'strict',
+      alwaysUseHttps: 'on',
+    },
+  }
+);
+
+const mangioCnameRecord = new cloudflare.Record('unstoppablemang.io-cname', {
+  name: domainName,
+  zoneId: unstoppableMangZoneId,
+  value: site.defaultHostname,
+  type: 'CNAME',
+  proxied: true,
+});
+
+const test = new cloudflare.Ruleset('mangio-redirect', {
+  zoneId: unstoppableMangZoneId,
+  name: 'Redirect mangio to mangoio',
+  kind: 'zone',
+  phase: 'http_request_dynamic_redirect',
+  rules: [
+    {
+      expression: '',
+      action: 'redirect',
+    },
+  ],
+});
+
+const test2 = new cloudflare.List('mangio-redirect', {
+  name: 'mangio_redirect',
+  accountId: '265a046434c952eeecb9710cfd76617c',
+  kind: 'redirct',
+  items: [
+    {
+    value: {
+      redirects: [
+        {
+          sourceUrl: mangioDomainName,
+          targetUrl: `https://${domainName}`,
+        }
+      ]
+    }
+    }
+  ]
 });
 
 export const validationToken = customDomain.validationToken;
