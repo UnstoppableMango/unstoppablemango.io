622310e70d5391ee354c507e907cc019d1dc2edf
Bungus McPherson <ecr.referee@hotmail.com>
2023-05-07 21:51:00 -0500

    Add some fun pages and links (#2)

---
+++ src/App/Navigable.fs
@@ -0,0 +1,20 @@
+module Navigable
+
+open Browser
+open Sutil
+open Sutil.Core
+open Sutil.CoreElements
+
+let bindHash<'T> (view: string -> SutilElement) =
+    let store = Store.make (window.location.hash)
+
+    fragment [
+        disposeOnUnmount [
+            store
+            Navigable.listenLocation (id, (fun l -> l.hash |> Store.set store))
+            |> Helpers.disposable
+        ]
+        Bind.el (store, view)
+    ]
+
+let nav route = window.location.hash <- route
