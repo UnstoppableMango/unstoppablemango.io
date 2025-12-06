d5cdc5548991fd1238d43ec80026490291ecdb27
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-12-06 13:08:25 -0600

    WIP

--- infra/index.ts
+++ infra/index.ts
@@ -4,30 +4,32 @@ import * as pulumi from '@pulumi/pulumi';
 import * as azure from '@pulumi/azure-native';
 import * as cloudflare from '@pulumi/cloudflare';
 
+const domainName = 'unstoppablemango.io';
+// const mangioDomainName = 'unstoppablemang.io';
+
 const resourceGroupName = 'UnstoppableMango.io';
 const resourceGroup = new azure.resources.ResourceGroup(resourceGroupName, {
   resourceGroupName,
-});
-
-const cdn = new azure.cdn.Profile('unstoppablemango-io', {
-  resourceGroupName: resourceGroup.name,
-  sku: {
-    name: azure.cdn.SkuName.Standard_Microsoft,
-  },
 });
 
 const storageAccount = new azure.storage.StorageAccount('website', {
   resourceGroupName: resourceGroup.name,
   kind: azure.storage.Kind.StorageV2,
   enableHttpsTrafficOnly: true,
+  allowBlobPublicAccess: true,
+  publicNetworkAccess: azure.storage.PublicNetworkAccess.Enabled,
+  minimumTlsVersion: azure.storage.MinimumTlsVersion.TLS1_2,
+  // customDomain: {
+  //   name: domainName,
+  // },
   sku: {
     name: azure.storage.SkuName.Standard_LRS,
   },
 });
 
 const staticWebsite = new azure.storage.StorageAccountStaticWebsite('website', {
-  accountName: storageAccount.name,
   resourceGroupName: resourceGroup.name,
+  accountName: storageAccount.name,
   indexDocument: 'index.html',
   error404Document: 'error.html',
 });
@@ -39,13 +41,20 @@ fs.readdirSync('../public', {
 }).forEach((dirent) => {
   if (!dirent.isFile()) return;
 
+  const assetPath = path.resolve(dirent.parentPath, dirent.name);
+  const relativePath = path.relative('../public', assetPath);
+  const ext = path.extname(dirent.name);
+
   new azure.storage.Blob(dirent.name, {
     resourceGroupName: resourceGroup.name,
     accountName: storageAccount.name,
     containerName: staticWebsite.containerName,
-    source: new pulumi.asset.FileAsset(path.resolve(dirent.parentPath, dirent.name)),
+    blobName: relativePath,
+    type: azure.storage.BlobType.Block,
+    accessTier: azure.storage.AccessTier.Cool,
+    source: new pulumi.asset.FileAsset(assetPath),
     contentType: ((): string => {
-      switch (path.extname(dirent.name)) {
+      switch (ext) {
         case '.html':
           return 'text/html';
         case '.css':
@@ -62,38 +71,45 @@ fs.readdirSync('../public', {
 export const originURL = storageAccount.primaryEndpoints.web;
 const originHostname = originURL.apply(x => new URL(x).hostname);
 
-const cdnEndpoint = new azure.cdn.Endpoint('cdn-endpoint', {
-  resourceGroupName: resourceGroup.name,
-  endpointName: 'unstoppablemango-io-endpoint',
-  profileName: cdn.name,
-  isHttpAllowed: false,
-  isHttpsAllowed: true,
-  isCompressionEnabled: true,
-  originHostHeader: originHostname,
-  origins: [{
-    name: storageAccount.name.apply((name) => `${name}-origin`),
-    hostName: originURL.apply(
-      (url) => url.replace('https://', '').replace('/', ''),
-    ),
-  }],
-  contentTypesToCompress: [
-    'text/html',
-    'text/css',
-    'application/javascript',
-    'image/png',
-    'image/jpeg',
-  ],
-});
+// const cdnEndpoint = new azure.cdn.Endpoint('cdn-endpoint', {
+//   resourceGroupName: resourceGroup.name,
+//   endpointName: 'unstoppablemango-io-endpoint',
+//   profileName: cdn.name,
+//   isHttpAllowed: false,
+//   isHttpsAllowed: true,
+//   isCompressionEnabled: true,
+//   originHostHeader: originHostname,
+//   origins: [{
+//     name: storageAccount.name.apply((name) => `${name}-origin`),
+//     hostName: originURL.apply(
+//       (url) => url.replace('https://', '').replace('/', ''),
+//     ),
+//   }],
+//   contentTypesToCompress: [
+//     'text/html',
+//     'text/css',
+//     'application/javascript',
+//     'image/png',
+//     'image/jpeg',
+//   ],
+// });
 
-const domainName = 'unstoppablemango.io';
-// const mangioDomainName = 'unstoppablemang.io';
 const unstoppableMangoZoneId = 'de10a9e5057761cf8b2151d80dd684fa';
 // const unstoppableMangZoneId = '24f3d181ad1b22a5e290175096171516';
+
+new azure.dns.RecordSet('test', {
+  resourceGroupName: resourceGroup.name,
+  zoneName: domainName,
+  recordType: 'CNAME',
+  cnameRecord: {
+    cname: originHostname,
+  },
+});
 
 new cloudflare.DnsRecord('unstoppablemango.io-cname', {
   name: domainName,
   zoneId: unstoppableMangoZoneId,
-  content: cdnEndpoint.hostName,
+  content: originHostname,
   type: 'CNAME',
   proxied: true,
   ttl: 1, // Automatic
@@ -112,5 +128,5 @@ new cloudflare.ZoneSetting('unstoppablemango.io-https', {
 });
 
 export { originHostname };
-export const cdnURL = pulumi.interpolate`https://${cdnEndpoint.hostName}`;
-export const cdnHostname = cdnEndpoint.hostName;
+// export const cdnURL = pulumi.interpolate`https://${cdnEndpoint.hostName}`;
+// export const cdnHostname = cdnEndpoint.hostName;
