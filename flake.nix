57509a10b364e6e00406997f39189d33a5b82854
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-12-19 11:20:39 -0600

    Nix flake systems (#559)

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
 
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
@@ -14,14 +15,9 @@
   outputs =
     inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
       imports = [ inputs.treefmt-nix.flakeModule ];
 
-      systems = [
-        "x86_64-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "aarch64-darwin"
-      ];
       perSystem =
         { pkgs, ... }:
         {
